b728e9ffd6b4d57d94812d279ad18cd5b5070e6c
selene <selineecl25@gmail.com>
2026-01-26 11:04:43 +0300

    hero changes

--- src/Components/FirstContent.jsx
+++ src/Components/FirstContent.jsx
@@ -10,18 +10,18 @@ const FirstContent = () => {
       <div className="first-left-lg d-none d-lg-flex flex-column min-vh-100">
         <h1 className="mt-5 ms-5 ps-2 pt-5">
           <span className="font-weight-light large-font">Welcome to</span><br />
-          <span className="font-weight-bold">ELIMUFITI - school resources &</span><br />
-          <span className="font-weight-bold">job connections™</span><br />
+          <span className="font-weight-bold">ELIMUFITI - Affordable School Learning Resources(CBE) </span><br />
+          
         </h1>
         <div className="ms-5 ps-2 pt-5">
           <h4>we provide the tools for academic success and</h4>
-          <h4>connect you with exciting job opportunities.</h4>
+          <h4>Affordable Learning & Revision Resources for Schools..</h4>
           <div className="d-flex">
             <Link to="/school/resources">
               <button className="btn btn-primary px-5 mx-2">School Resources</button>
             </Link>
-              <Link to="/careers">
-              <button className="btn btn-primary px-5 mx-2">Jobs</button>
+              <Link to="/signup">
+              <button className="btn btn-primary px-5 mx-2">Get Started</button>
             </Link>
             
           </div>
@@ -32,22 +32,24 @@ const FirstContent = () => {
       <div className="first-left-sm d-flex d-lg-none flex-column min-vh-100">
       <h1 className="mt-4 ms-2 ps-2 pt-3 text-charcoal text-center">
         <span className="font-weight-light-lg large-font-sm text-charcoal">Welcome to</span><br /><br/>
-        <span className="font-weight-bold-sm text-charcoal">Your hub for school resources &</span><br />
-        <span className="font-weight-bold-sm text-charcoal">job connections™</span><br />
+        <span className="font-weight-bold-sm text-charcoal">ELIMUFITI - Affordable School Learning Resources(CBE) </span><br />
+        
       </h1>
 
         <div className="ms-2 ps-2 pt-2 accursed-black font-weight-medium-sm">
-        <div className="text-center mt-4 mb-4">
-          <h4>We provide the tools for academic success and</h4>
-          <h4>connect you with exciting job opportunities.</h4>
+        <div className="text-center my-5 px-3">
+        <h3 className="fw-bold text-primary mb-3">We provide</h3>
+        <h4 className="fw-semibold mb-2">Tools that support academic success for schools,</h4>
+        <h4 className="fw-semibold text-muted">teachers, parents, and guardians —</h4>
         </div>
+
 
         <div className="d-flex mt-5 pt-2">
           <Link to="/school/resources">
             <button className="btn btn-primary px-2 mx-2 small-width-btn">School Resources</button>
           </Link>
-            <Link to="/careers">
-            <button className="btn btn-primary px-2 mx-2 small-width-btn">Jobs</button>
+            <Link to="/signup">
+            <button className="btn btn-primary px-2 mx-2 small-width-btn">Get Started</button>
           </Link>
           
         </div>
